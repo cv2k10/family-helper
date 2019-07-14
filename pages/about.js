7011c4c96d4666e99a060c33bf7c9d951e3e7207
Calvin <cv2k10@gmail.com>
2019-07-14 16:54:04 +0800

    add postcss with autoprefixer, Head on page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
+import Head from 'next/head';
+
 const About = () => (
   <div>
+        <Head>
+      <title>About Us</title>
+    </Head>
   <h1>About us</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae dolores libero iusto nulla. Odit, ex? Placeat architecto praesentium voluptas, sapiente optio qui blanditiis mollitia provident voluptates nihil eveniet. Aliquid, illo.</p>
   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import NavBar from '../components/NavBar';
+import Head from 'next/head'
 import "../scss/style.scss"
 
 const Index = () => (
   <section>
+    <Head>
+      <title>Home</title>
+    </Head>
     <NavBar />
     <h1>Hellow World from Next JS</h1>
   </section>
