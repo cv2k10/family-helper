cd67591e5b9aa2030faf23fa3756bebc5cb65123
Calvin <cv2k10@gmail.com>
2019-09-26 21:24:59 +0800

    start develop from Ubuntu, solve scss loading issur on pages

--- pages/ContactUs.js
+++ pages/ContactUs.js
@@ -19,13 +19,14 @@ class ContactUs extends React.Component {
             <h2>Any Enquiries</h2>
             <section className="content">
               <p>Forward your request or call us</p>
-              <p>03-92677753</p>
-              <p>019-2791168</p>
+              <p>Office phone: <a href="tel:603926778753">603-92677 8753</a></p>
+              <p>Mobile: <a href="tel:60192791168">6019-279 1168</a></p>
 
               <p>Monday To Saturday: 9am - 6pm<br/>Sunday and Public holiday: 11am - 3am</p>
 
               <p>Our FAQ will give you fast and accurate info and answer, refer it now.</p>
             </section>
+
           </div>
         </Layout>
 
