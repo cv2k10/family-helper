cea304eb289985d8586488c566c9b750892edbeb
Calvin <cv2k10@gmail.com>
2019-11-03 15:41:09 +0800

    change to dw mail to test send from do

--- server/index.js
+++ server/index.js
@@ -43,39 +43,39 @@ app.prepare().then(() => {
 
     const nodemailer = require("nodemailer");
 
-    // async..await is not allowed in global scope, must use a wrapper
-    async function main() {
+    // // async..await is not allowed in global scope, must use a wrapper
+    // // async function main() {
 
-      // Generate test SMTP service account from ethereal.email
-      // Only needed if you don't have a real mail account for testing
-      // let testAccount = await nodemailer.createTestAccount();
+    //   // Generate test SMTP service account from ethereal.email
+    //   // Only needed if you don't have a real mail account for testing
+    //   // let testAccount = await nodemailer.createTestAccount();
 
-      // create reusable transporter object using the default SMTP transport
+    //   // create reusable transporter object using the default SMTP transport
+    //   let transporter = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     // secure: 'gmail', // true for 465, false for other ports
+    //     auth: {
+    //       user: 'lfei2k@gmail.com', // generated ethereal user
+    //       pass: 'k2iBtrSc1' // generated ethereal password
+    //     },
+    //     tls: {
+    //       rejectUnauthorized: false
+    //     }       
+    //   });
+
+      async function main() {
       let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
+        host: 'mail.deltaweb.com.my',
         port: 465,
         secure: true,
         // secure: 'gmail', // true for 465, false for other ports
         auth: {
-          user: 'lfei2k@gmail.com', // generated ethereal user
-          pass: 'k2iBtrSc1' // generated ethereal password
-        },
-        tls: {
-          rejectUnauthorized: false
-        }       
+          user: 'webtest@deltaweb.com.my', // generated ethereal user
+          pass: 'btrsc1' // generated ethereal password
+        }     
       });
-
-      // async function main() {
-      // let transporter = nodemailer.createTransport({
-      //   host: 'mail.deltaweb.com.my',
-      //   port: 465,
-      //   secure: true,
-      //   // secure: 'gmail', // true for 465, false for other ports
-      //   auth: {
-      //     user: 'webtest@deltaweb.com.my', // generated ethereal user
-      //     pass: 'btrsc1' // generated ethereal password
-      //   }     
-      // });
       
       // send mail with defined transport object
       let info = await transporter.sendMail(
