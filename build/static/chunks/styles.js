8079908054cf1a521bd0b54265fe84af9a3db91d
Calvin <cv2k10@gmail.com>
2020-02-21 15:06:35 +0800

    tuning index page

--- build/static/chunks/styles.js
+++ build/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1581060578056");
+            injectCss(link, link.href.split("?")[0] + "?unix=1582188623397");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1581135760327");
+            injectCss(link, link.href.split("?")[0] + "?unix=1582189588320");
           });
       }
     }
--- build/static/runtime/webpack.js
+++ build/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b16e4636de362e16576b";
+/******/ 	var hotCurrentHash = "8c38ccca7061879a1f0c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
