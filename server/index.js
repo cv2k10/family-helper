2ff565b6359d93f9e61926b0e7aa45ce376b1e63
Calvin <cv2k10@gmail.com>
2020-03-15 16:03:55 +0800

    set views folder into server folder

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const next = require('next');
+const path = require('path');
 const logger = require('morgan');
 const layouts = require('express-ejs-layouts');
 require("dotenv").config();
+
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -17,6 +19,7 @@ app.prepare().then(() => {
   server.use(express.urlencoded({ extended: false }));
   server.use(express.json());
 
+  server.set('views', path.join(__dirname, 'views'));
   server.set('view engine', 'ejs');
   server.use(layouts);
 
