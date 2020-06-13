a87cbe3c2ead52c8d2e785af8e9758aefebd38f8
Calvin <cv2k10@gmail.com>
2020-06-13 12:19:17 +0800

    split keys into keyProduction and keyDevelopment in keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
-module.exports = {
+const keysProduction = {
   facebook: {
     appID: "258569651891377",
     appSecret: "bf3f3d73b819deb076c8c1b2dc17816e",
@@ -10,3 +10,20 @@ module.exports = {
     cookieKey: "everybodylivesinawonderfulworld",
   },
 };
+
+const keysDevelopment = {
+  facebook: {
+    appID: "675388803304459",
+    appSecret: "8fa2869b8793ff4b4ab031552a5eb7a7",
+  },
+  mongodb: {
+    dbURI: "mongodb://cv2k10:btrsc1@ds115340.mlab.com:15340/family-helper1",
+  },
+  session: {
+    cookieKey: "everybodylivesinawonderfulworld",
+  },
+};
+
+const dev = process.env.NODE_ENV !== "production";
+
+module.exports = dev? keysDevelopment: keysProduction;
